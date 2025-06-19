75cf1ccd1b55013020aa0274cee1a82ab38a793c
Atif Niyaz <147269431+Atif962@users.noreply.github.com>
2025-06-19 18:10:04 +0530

    Add files via upload

---
+++ MultipleFunction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void england(){
+    printf("you are in England\n");
+    return;
+}
+void australia(){
+    printf("you are in australia");
+    return;
+}
+void india(){
+    printf("are you from India");
+    return;
+}
+int main()
+{
+    india();
+    england();
+    india();
+
+}
